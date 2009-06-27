22fe7b18ab32c0c074806b98c758d7f735989a63
Solomon Bothwell <solomon@yellowshark.gateway.2wire.net>
2009-06-27 02:54:37 -0700

    changed painting sizes

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,5 +1,5 @@
 class Painting < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :small => "125x125>", :medium => "450x450>" },
+  has_attached_file :photo, :styles => { :small => "125x125>", :medium => "700x700>" },
                     :url  => "/assets/products/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
 
