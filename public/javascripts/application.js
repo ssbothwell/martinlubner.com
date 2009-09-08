0f07473062d7ac3ca39c4015def2426e5c64c59e
Solomon Bothwell <solomon@yellowshark.gateway.2wire.net>
2009-09-07 18:59:52 -0700

    added blueprint

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,25 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+// test auto-ready logic - call corner before DOM is ready
+$('#readyTest').corner();
+
+$(function(){
+$('#dynCorner').click(function() {
+	$('#dynamic').corner();
+});
+$('#dynUncorner').click(function() {
+	$('#dynamic').uncorner();
+});
+
+    $('div.inner').wrap('<div class="outer"></div>');
+    $('p').wrap("<code></code>");
+
+    $('div.demo, div.inner').each(function() {
+        var t = $('p', this).text();
+        eval(t);
+    });
+
+    // fixed/fluid tests
+    $("div.box").corner("keep");
+    $("#abs").corner("cc:#08e");
+});
