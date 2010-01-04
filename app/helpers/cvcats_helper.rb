d571d244d558bceb4a02ecefe5c7a122353ec3f7
Solomon Bothwell <solomon@solomon-bothwells-macbook-pro-15.local>
2010-01-03 22:13:15 -0800

    added o/c notice

--- app/helpers/cvcats_helper.rb
+++ app/helpers/cvcats_helper.rb
@@ -1,22 +1,42 @@
 module CvcatsHelper
-  def add_task_link(form_builder)
-    link_to_function 'add an entry' do |page|
-      form_builder.fields_for :cventries, Cventry.new, :child_index => 'NEW_RECORD' do |form|
-        html = render(:partial => 'entry', :locals => { :entry_form => form })
-        page << "$('entries').insert({ bottom: '#{escape_javascript(html)}'.replace(/NEW_RECORD/g, new Date().getTime()) });"
-      end
-    end
+#  def add_task_link(form_builder)
+#    link_to_function 'add an entry' do |page|
+#      form_builder.fields_for :cventries, Cventry.new, :child_index => 'NEW_RECORD' do |form|
+#        html = render(:partial => 'entry', :locals => { :entry_form => form })
+#        page << "$('entries').append('#{escape_javascript(html)}');"
+#      end
+#    end
+#  end
+#  def remove_entry_link(form_builder)
+#    if form_builder.object.new_record?
+#      # If the task is a new record, we can just remove the div from the dom
+#    link_to_function("remove", "$(this).up('.entry').remove();");
+#    else
+#      # However if it's a "real" record it has to be deleted from the database,
+#      # for this reason the new fields_for, accept_nested_attributes helpers give us _delete,
+#      # a virtual attribute that tells rails to delete the child record.
+#    form_builder.hidden_field(:_delete) +
+#    link_to_function("remove", "$(this).up('.entry').hide(); $(this).previous().value = '1'")
+#    end
+#  end
+
+  def add_entry_link(name, association)
+    link_to(name, "javascript:void(0)", :class => "add_entry", :"data-association" => association)
+  end  
+  
+  def remove_entry_link(name, entry_form)
+    entry_form.hidden_field(:_delete) + link_to(name, "javascript:void(0)", :class => "remove_child")
   end
-  def remove_entry_link(form_builder)
-    if form_builder.object.new_record?
-      # If the task is a new record, we can just remove the div from the dom
-    link_to_function("remove", "$(this).up('.entry').remove();");
-    else
-      # However if it's a "real" record it has to be deleted from the database,
-      # for this reason the new fields_for, accept_nested_attributes helpers give us _delete,
-      # a virtual attribute that tells rails to delete the child record.
-    form_builder.hidden_field(:_delete) +
-    link_to_function("remove", "$(this).up('.entry').hide(); $(this).previous().value = '1'")
+  
+  def new_entry_fields_template(form_builder, association, options = {})
+    options[:object] ||= form_builder.object.class.reflect_on_association(association).klass.new
+    options[:partial] ||= association.to_s.singularize
+    options[:form_builder_local] ||= :f
+    
+    content_tag(:div, :id => "#{association}_fields_template", :style => "display: none") do
+      form_builder.fields_for(association, options[:object], :child_index => "new_#{association}") do |f|
+        render(:partial => options[:partial], :locals => {options[:form_builder_local] => f})
+      end
     end
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,25 +1,20 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-// test auto-ready logic - call corner before DOM is ready
-$('#readyTest').corner();
-
-$(function(){
-$('#dynCorner').click(function() {
-	$('#dynamic').corner();
-});
-$('#dynUncorner').click(function() {
-	$('#dynamic').uncorner();
-});
-
-    $('div.inner').wrap('<div class="outer"></div>');
-    $('p').wrap("<code></code>");
-
-    $('div.demo, div.inner').each(function() {
-        var t = $('p', this).text();
-        eval(t);
-    });
-
-    // fixed/fluid tests
-    $("div.box").corner("keep");
-    $("#abs").corner("cc:#08e");
+$(function() {
+  $('form a.add_child').click(function() {
+    var assoc   = $(this).attr('data-association');
+    var content = $('#' + assoc + '_fields_template').html();
+    var regexp  = new RegExp('new_' + assoc, 'g');
+    var new_id  = new Date().getTime();
+        
+    $(this).parent().before(content.replace(regexp, new_id));    
+    return false;
+  });
+  
+  $('form a.remove_child').live('click', function() {
+    var hidden_field = $(this).prev('input[type=hidden]')[0];
+    if(hidden_field) {
+      hidden_field.value = '1';
+    }
+    $(this).parents('.fields').hide();
+    return false;
+  });
 });
